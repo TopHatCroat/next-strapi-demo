405edc5942dc344e1f8a5099ad0e4e52fa5a824e
Antonio Martinović <antonio.martinovic@pm.me>
2023-06-20 20:15:00 +0200

    Add OpenAPI documentation to backend

--- apps/backend/config/plugins.ts
+++ apps/backend/config/plugins.ts
@@ -9,4 +9,35 @@ export default ({ env }) => ({
       },
     },
   },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '0.1.0',
+        title: 'Strapi CMS Demo',
+        description: 'Strapi CMS Demo API Documentation',
+        contact: {
+          name: 'TEAM',
+          email: 'contact-email@something.io',
+          url: 'mywebsite.io'
+        },
+        license: {
+          name: 'MIT',
+          url: 'https://opensource.org/license/mit/'
+        },
+      },
+      'x-strapi-config': {
+        // Leave empty to ignore plugins during generation
+        plugins: [ 'upload', 'users-permissions'],
+        path: '/documentation',
+      },
+      servers: [{ url: 'http://localhost:1337/api', description: 'Development server' }],
+      externalDocs: {
+        description: 'Find out more',
+        url: 'https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html'
+      },
+      security: [ { bearerAuth: [] } ]
+    }
+  }
 });
